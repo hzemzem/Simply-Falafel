f38f416516712f9b6851b571d986fccdcf88ad4b
davesrose <dsr0116@yahoo.com>
2017-08-23 18:07:01 -0400

    made a passwords.js for storing local password

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,12 +1,10 @@
 var path = require("path");
 
-var db = require("../models");
-
 module.exports = function(app) {
 
-	app.get("/", function(req, res) {
-		res.sendFile(path.join(__dirname, "../public/index.html"));
-	});
+  app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+  });
 
 	app.get('/menu', function(req, res) {
 		db.MenuItem.findAll({}).then(function(response){
